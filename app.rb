1fd307ffb8177e3864351a0dbedae14d90a85b17
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-18 17:25:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-      get '/newteam'
-    
+      get '/newteam' do
+        erb :newteam
+      end
+      
+      post 'post' do
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @pf = params[:pf]
+        @sf = params[:sf]
+        @c = params[:c]
+        erb :team
+      end
 end
